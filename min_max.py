2545fe46b90f6d9f4d2933e914cd47cf237a951e
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-01-19 22:12:18 -0800

    Update

--- min_max.py
+++ min_max.py
@@ -1,12 +1,12 @@
-Num = int(input("How many integers would you like to enter?"))
+Num=int(input("How many integers would you like to enter?"))
 print("Please enter",Num, "integers.")
-min = int(input())
-max = min
+min=int(input())
+max=min
 for i in range(1,Num):
-    number = int(input())
+    number=int(input())
     if number > max:
-        max = number
+        max=number
     if number < min:
-        min = number
+        min=number
 print("min:", min)
 print("max:", max)
